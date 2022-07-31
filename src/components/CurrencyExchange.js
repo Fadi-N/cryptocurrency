f8308e50cc091ece60ef55b8f3cbb4868dd9a222
fadinachawati <fadinachawaati17@gmail.com>
2022-07-31 11:05:49 +0200

    Initial commit

--- src/components/CurrencyExchange.js
+++ src/components/CurrencyExchange.js
@@ -6,17 +6,11 @@ function CurrencyExchange() {
     const currenciesSymbols = ['None', 'PLN', 'EUR', 'USD']
     const [exchangeFrom, setExchangeFrom] = useState(null)
     const [exchangeTo, setExchangeTo] = useState(null)
-    const [amountFrom, setAmountFrom] = useState(1)
-    const [amountTo, setAmountTo] = useState(0)
-    const [finalAmount, setFinalAmount] = useState(0)
-    /*
-    console.log(exchangeFrom)
-    console.log(exchangeTo)
-    console.log(amountFrom)
-    console.log(amountTo)
-    */
+    const [finalAmountFromTo, setFinalAmountFromTo] = useState('')
+    const [finalAmountToFrom, setFinalAmountToFrom] = useState('')
 
-    const convert = () => {
+    const convertFromTo = (e) => {
+        setFinalAmountToFrom(e.target.value)
         const options = {
             method: 'GET',
             url: 'https://alpha-vantage.p.rapidapi.com/query',
@@ -31,41 +25,72 @@ function CurrencyExchange() {
             }
         }
 
-        axios.request(options).then((response) => {
-            let exchangeRate = response.data['Realtime Currency Exchange Rate']['5. Exchange Rate']
-            setFinalAmount(exchangeRate * amountFrom)
-        }).catch((error) => {
-            console.error(error)
-        })
+        e.target.value === '' ? setFinalAmountFromTo('') : (
+            axios.request(options).then((response) => {
+                let exchangeRate = response.data['Realtime Currency Exchange Rate']['5. Exchange Rate']
+                setFinalAmountFromTo(exchangeRate * e.target.value)
+            }).catch((error) => {
+                console.error(error)
+            }))
+    }
+
+    const convertToFrom = (e) => {
+        setFinalAmountFromTo(e.target.value)
+        const options = {
+            method: 'GET',
+            url: 'https://alpha-vantage.p.rapidapi.com/query',
+            params: {
+                from_currency: `${exchangeTo}`,
+                function: 'CURRENCY_EXCHANGE_RATE',
+                to_currency: `${exchangeFrom}`
+            },
+            headers: {
+                'X-RapidAPI-Key': 'a81410a48fmshde406f56ec3622ep12f667jsnf6d259e85b24',
+                'X-RapidAPI-Host': 'alpha-vantage.p.rapidapi.com'
+            }
+        }
+        e.target.value === '' ? setFinalAmountToFrom('') : (
+            axios.request(options).then((response) => {
+                let exchangeRate = response.data['Realtime Currency Exchange Rate']['5. Exchange Rate']
+                setFinalAmountToFrom(exchangeRate * e.target.value)
+            }).catch((error) => {
+                console.error(error)
+            })
+        )
     }
 
     return (
         <div>
-            <input type="number" value={amountFrom} onChange={(e) => setAmountFrom(e.target.value)} placeholder="from"/>
+            <input type="number" value={finalAmountToFrom} onChange={(e) => convertFromTo(e)} placeholder="from"/>
             <select name="from" onChange={
                 (e) => e.target.value === "None" ? setExchangeFrom(null) : setExchangeFrom(e.target.value)
             }>
                 {
                     currenciesSymbols.map((currencySymbol, index) => {
                         return (
-                            <option key={index}>{currencySymbol}</option>
+                            exchangeTo === currencySymbol ?
+                                <option key={index} style={{display: "none"}} disabled>{currencySymbol}</option> : (
+                                    <option key={index}>{currencySymbol}</option>
+                                )
                         )
                     })
                 }
             </select>
-            <input type="number" value={finalAmount} onChange={(e) => setAmountTo(e.target.value)} placeholder="to"/>
+            <input type="number" value={finalAmountFromTo} onChange={(e) => convertToFrom(e)} placeholder="to"/>
             <select name="to" onChange={
                 (e) => e.target.value === "None" ? setExchangeTo(null) : setExchangeTo(e.target.value)
             }>
                 {
                     currenciesSymbols.map((currencySymbol, index) => {
                         return (
-                            <option key={index}>{currencySymbol}</option>
+                            exchangeFrom === currencySymbol ?
+                                <option key={index} style={{display: "none"}} disabled>{currencySymbol}</option> : (
+                                    <option key={index}>{currencySymbol}</option>
+                                )
                         )
                     })
                 }
             </select>
-            <button onClick={convert}>Convert</button>
         </div>
     );
 }
